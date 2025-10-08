98aa73aaaca2e6e163af0fb997cf9dee7b65966f
Muhless <nuryadi.muhta@gmail.com>
2025-10-08 10:46:51 +0700

    style(homescreen): add profile header

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,21 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:pg_pritani/screens/profile_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Halaman Utama"), ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const ProfileScreen()),
-            );
-          },
-          child: const Text("Ke Halaman Profil"),
+      body: SafeArea(
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Halo",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Text(
+                    "Admin",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.amber,
+                    ),
+                  ),
+                ],
+              ),
+
+              const SizedBox(width: 12),
+
+              Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.lightBlue,
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                clipBehavior: Clip.antiAlias,
+                child: ClipOval(
+                  child: Image(
+                    image: AssetImage('assets/icons/businessman.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          
         ),
       ),
     );
